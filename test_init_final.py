9091741cecd377450b1ef95ee071aba239969278
voicestory776 <52654457+voicestory776@users.noreply.github.com>
2020-06-23 23:29:12 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -564,7 +564,7 @@ async def task():
 					#print ('if ', bossTime[i])
 					bossMungFlag[i] = True
 					tmp_bossTime[i] = bossTime[i]
-					tmp_bossTimeString[i] = tmp_bossTime[i].strftime('%H:%M:%S')
+					tmp_bossTimeString[i] = tmp_bossTime[i].strftime('%H:%M')
 					tmp_bossDateString[i] = tmp_bossTime[i].strftime('%Y-%m-%d')
 					bossTimeString[i] = '99:99:99'
 					bossDateString[i] = '9999-99-99'
